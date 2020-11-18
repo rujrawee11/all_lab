487f4a57c580bd2409e19a87a4ac1e9ef7c7af65
rujrawee11 <73680402+rujrawee11@users.noreply.github.com>
2020-11-18 21:41:42 +0700

    Update dbconfig.php

--- dbconfig.php
+++ dbconfig.php
@@ -1,8 +1,8 @@
 <?php
 return array(
-    'host' => 'host',
-    'username' => 'username',
-    'password' => 'password',
-    'database' => 'databasename',
+    'host' => 'rujrawee123.mysql.database.azure.com', 
+    'username' => 'rujrawee@rujrawee',
+    'password' => 'Neaybaby123',
+    'database' => 'rujrawee@rujrawee123',
 );
 ?>
