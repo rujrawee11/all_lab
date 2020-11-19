85de3ef35b0e6463ec864b331ff243971b00b871
rujrawee11 <73680402+rujrawee11@users.noreply.github.com>
2020-11-19 14:20:03 +0700

    Delete dbconfig.php

--- dbconfig.php
+++
@@ -1,8 +0,0 @@
-<?php
-return array(
-    'host' => 'rujrawee123.mysql.database.azure.com', 
-    'username' => 'rujrawee@rujrawee123',
-    'password' => 'Neaybaby123',
-    'database' => 'rujrawee123',
-);
-?>
