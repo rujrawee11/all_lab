c73a58c3819cb43214e4a3e7fc64c0ce6ac3abe9
rujrawee11 <73680402+rujrawee11@users.noreply.github.com>
2020-11-19 15:49:57 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -23,8 +23,8 @@ $sql = "INSERT INTO guestbook (Name , Comment , Link) VALUES ('$name', '$comment
 
 if (mysqli_query($conn, $sql)) {
     echo '<div class="container">
-            <h3>Comment has been saved in to the database successfully.</h3>
-            <a role="button" class="btn btn-primary mt-3" href="guestbook.php">Home</a>
+            <h3>Comment has been saved.</h3>
+            <a role="button" class="btn btn-success" href="guestbook.php">Home</a>
          </div>';
 } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
