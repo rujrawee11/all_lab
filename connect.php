bd2d491cbf97110243a9a23a0da78a847c9546a3
rujrawee11 <73680402+rujrawee11@users.noreply.github.com>
2020-11-27 12:41:42 +0700

    Delete connect.php

--- connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = mysqli_init();
-mysqli_real_connect($conn, 'rujrawee1.mysql.database.azure.com', 'rujawee11@rujrawee1', 'Neaybaby123', 'Test');
-if(mysqli_connect_errno($conn)){
-    echo "connect fail".mysqli_connect_error();
-}
-?>
