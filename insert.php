aeed41fa3d2282dad75923b1b92969cd0b64095c
rujrawee11 <73680402+rujrawee11@users.noreply.github.com>
2020-11-27 12:49:19 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -4,13 +4,14 @@ require('connect.php');
         $num = $_POST['name'];
         $numa = $_POST['height'];
         $numb = $_POST['weight'];
-        $numc = $numa / ($numb**2);
+        $numc = ($numa / ($numb**2));
 
         $sql = "INSERT INTO test (name, height, weight, bmi) value ('$num','$numa', '$numb', '$numc')";
 
-        if(mysqli_query($conn, $sql)){
-            echo "New data insert successfully";
-            echo "<a href='index.php'>back to home page</a>";
+        <div class="mt-4">
+            <button type="submit" class="btn btn-dark">Submit</button>
+            <a role="button" class="btn btn-dark" href="index.php">Back</a>
+        </div>
         }
     }
 
